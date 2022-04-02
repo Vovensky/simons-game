a8fd028e3fec04291adcee385e544e9654d674cd
Vladimir <urinskii1996@gmail.com>
2022-04-02 16:27:18 +0500

    second commit

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/simons-play-game/'
+    : '/'
 })
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/simons-play-game/'
+    : '/'
+}
 module.exports = {
   configureWebpack: {
     devtool: 'source-map'
