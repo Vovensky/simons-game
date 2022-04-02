665ec714d4dc3df81fd1cff55394104d94e18db7
Vovensky <102856478+Vovensky@users.noreply.github.com>
2022-04-02 17:01:44 +0500

    set gh-pages branch

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,17 @@
 #!/usr/bin/env sh
 
-# остановить публикацию при ошибках
 set -e
 
-# сборка
 npm run build
 
-# переход в каталог сборки
 cd dist
 
-# если вы публикуете на пользовательский домен
-# echo 'www.example.com' > CNAME
+
 
 git init
 git add -A
-git commit -m 'deploy'
+git commit -m 'New Deployment'
 
-# если вы публикуете по адресу https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
-git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-
+git push -f git@github.com:vovensky/simons-play-game.git main:gh-pages
+ 
 cd -
--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,3 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/simons-play-game/'
-    : '/'
-})
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/simons-play-game/'
-    : '/'
-}
-module.exports = {
-  configureWebpack: {
-    devtool: 'source-map'
-  }
+  publicPath: '/simons-play-game/'
 }
